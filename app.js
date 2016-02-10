45c4dbbf6e3004b9c8dfba95051048d43eb42c5d
benjaminspalding <benjamin.spalding02@gmail.com>
2016-02-10 14:16:41 -0500

    4.7

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
 (function() {
   var app = angular.module('gemStore', []);
 
-  app.controller('GalleryController', function(){
-    this.current = 0;
-    this.setCurrent = function(imageNumber){
-      this.current = imageNumber || 0;
-    };
-  });
+
+
+app.directive('productGallery', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'product-gallery.html',
+    controller: function() {
+      this.current = 0;
+      this.setCurrent = function(imageNumber) {
+        this.current = imageNumber || 0;
+      };
+    },
+    controllerAs: 'gallery'
+  };
+});
 
   app.controller('StoreController', function() {
     this.products = gems;
   });
-
-
-app.directive('productTabs', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'product-tabs.html',
-    controller: function() {
-      this.tab = 1;
-
-      this.isSet = function(checkTab) {
-        return this.tab === checkTab;
-      };
-
-      this.setTab = function(activeTab) {
-        this.tab = activeTab;
-      };
-    },
-    controllerAs: 'tab'
-  };
-});
 
   app.controller("ReviewController", function(){
 
@@ -43,7 +32,7 @@ app.directive('productTabs', function() {
 
   });
 
-  app.directive("productDescription", function() {
+  app.directive("productDescriptions", function() {
     return {
       restrict: 'E',
       templateUrl: "product-description.html"
@@ -61,6 +50,25 @@ app.directive('productTabs', function() {
     return {
       restrict:"A",
       templateUrl: "product-specs.html"
+    };
+  });
+
+  app.directive("productTabs", function() {
+    return {
+      restrict: "E",
+      templateUrl: "product-tabs.html",
+      controller: function() {
+        this.tab = 1;
+
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
+
+        this.setTab = function(activeTab) {
+          this.tab = activeTab;
+        };
+      },
+      controllerAs: "tab"
     };
   });
 
