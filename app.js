df24fea11e6c8496c45f75db005750eb7eb8b5c7
benjaminspalding <benjamin.spalding02@gmail.com>
2016-02-10 14:09:57 -0500

    4.6

--- app.js
+++ app.js
@@ -12,17 +12,25 @@
     this.products = gems;
   });
 
-  app.controller("TabController", function() {
-    this.tab = 1;
 
-    this.isSet = function(checkTab) {
-      return this.tab === checkTab;
-    };
+app.directive('productTabs', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'product-tabs.html',
+    controller: function() {
+      this.tab = 1;
 
-    this.setTab = function(setTab) {
-      this.tab = setTab;
-    };
-  });
+      this.isSet = function(checkTab) {
+        return this.tab === checkTab;
+      };
+
+      this.setTab = function(activeTab) {
+        this.tab = activeTab;
+      };
+    },
+    controllerAs: 'tab'
+  };
+});
 
   app.controller("ReviewController", function(){
 
@@ -35,13 +43,26 @@
 
   });
 
-  app.directive("productDescription", function(){
-    return{
+  app.directive("productDescription", function() {
+    return {
       restrict: 'E',
       templateUrl: "product-description.html"
-    };   
+    };
   });
-  
+
+  app.directive("productReviews", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-reviews.html"
+    };
+  });
+
+  app.directive("productSpecs", function() {
+    return {
+      restrict:"A",
+      templateUrl: "product-specs.html"
+    };
+  });
 
   var gems = [
     {
